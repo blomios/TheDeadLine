00c546d516d429e37dea9bfc243d8ba33ff6e8b1
Blomios <blomios@gmail.com>
2019-05-01 13:46:08 +0200

    InitialCommit
    Some trap created
    Player controller work in progress

---
+++ Assets/Scipts/FalseGround.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FalseGround : Trap
+{
+
+    public Rigidbody2D rb;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        }
+    }
+}
---
+++ Assets/Scipts/Spades.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spades: Trap
+{
+    public Animator animator;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("enter");
+        if (collision.gameObject.tag == "Player" && isActive)
+        {
+            Debug.Log("player");
+            animator.SetTrigger("Enter");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && isActive)
+        {
+            animator.SetTrigger("Exit");
+        }
+    }
+}
